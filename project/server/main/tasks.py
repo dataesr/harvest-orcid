620e9735bdc9630130b75d60a6b92056c91e816a
eric <eric.jeangirard@recherche.gouv.fr>
2022-07-18 17:01:50 +0200

    bugfix

--- project/server/main/tasks.py
+++ project/server/main/tasks.py
@@ -23,27 +23,27 @@ def to_json(input_list, output_file, ix):
             json.dump(entry, outfile)
 
 def create_task_dois(arg):
-    cmd1 = 'echo doi > /upw_data/dois_from_orcid.csv'
-    os.system(cmd1)
-    cmd2 = 'rm -rf /upw_data/orcid_idref.jsonl'
+    cmd2 = 'rm -rf /upw_data/orcid_tmp.jsonl'
     os.system(cmd2)
 
     for i1 in range(0, 10):
         for i2 in range(0, 10):
             for i3 in range(0, 10):
-                cmd1 = f"cat /upw_data/links_publications_persons/links_orcid0000-00{i1}{i2}-{i3}* | jq -r .publi_id | grep '^doi10.' | grep -v ',' | cut -c 4- | sort -u >> /upw_data/dois_from_orcid.csv"
-                os.system(cmd1)
-                cmd2 = f"cat /upw_data/links_publications_persons/links_orcid0000-00{i1}{i2}-{i3}* >> /upw_data/orcid_idref.jsonl"
+                #cmd1 = f"cat /upw_data/links_publications_persons/links_orcid0000-00{i1}{i2}-{i3}* | jq -r .publi_id | grep '^doi10.' | grep -v ',' | cut -c 4- | sort -u >> /upw_data/dois_from_orcid.csv"
+                #os.system(cmd1)
+                cmd2 = f"cat /upw_data/links_publications_persons/links_orcid0000-00{i1}{i2}-{i3}* >> /upw_data/orcid_tmp.jsonl"
                 os.system(cmd2)
-    logger.debug('getting dois from orcid links')
-    output_file = '/upw_data/dois_from_orcid.json'
-    df = pd.read_csv('/upw_data/dois_from_orcid.csv', chunksize = 20000)
-    ix = 0
-    for c in df:
-        dois = c.doi.tolist()
-        to_json(dois, output_file, ix)
-        ix += 1
-    with open(output_file, 'a') as outfile:
-        outfile.write(']')
-    upload_object('publications-related', output_file, f'dois_from_orcid.json')
+    cmd4 = "sed -e 's/}{/}\\n{/g' /upw_data/orcid_tmp.jsonl > /upw_data/orcid_idref.jsonl"
+    os.system(cmd4)
+    #logger.debug('getting dois from orcid links')
+    #output_file = '/upw_data/dois_from_orcid.json'
+    #df = pd.read_csv('/upw_data/dois_from_orcid.csv', chunksize = 20000)
+    #ix = 0
+    #for c in df:
+    #    dois = c.doi.tolist()
+    #    to_json(dois, output_file, ix)
+    #    ix += 1
+    #with open(output_file, 'a') as outfile:
+    #    outfile.write(']')
+    #upload_object('publications-related', output_file, f'dois_from_orcid.json')
     upload_object('misc', '/upw_data/orcid_idref.jsonl', f'orcid_idref.jsonl')
